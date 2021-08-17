ea641dd6746123a18f20ac18959af45fa1cad38b
yuexing0921 <yuexing0921@gmail.com>
2021-08-17 16:32:47 +0800

    feat: 添加stack

--- LeetCode/20.js
+++ LeetCode/20.js
@@ -1,9 +1,33 @@
 // https://leetcode-cn.com/problems/valid-parentheses/
 
 /**
+ * 使用栈来运算-用数组模拟栈
  * @param {string} s
  * @return {boolean}
  */
 var isValid = function (s) {
-    
+  if (s.length % 2 === 1) {
+    return false;
+  }
+  const stack = [];
+  for (var i = 0; i < s.length; i++) {
+    const c = s.charAt(i);
+    if (c === "{" || c === "[" || c === "(") {
+      stack.push(c);
+    } else {
+      // peek一个
+      const peek = stack[stack.length - 1];
+      const str = peek + c;
+      if (str === "{}" || str === "()" || str === "[]") {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+    // if (/\{|\(|\[/.test(c)) {
+    //   stack.push(c);
+    // }
+  }
+
+  return stack.length === 0;
 };
