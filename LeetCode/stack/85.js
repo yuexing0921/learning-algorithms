7545ffe299824a74f39c6149fc37160217817764
yuexing0921 <yuexing0921@gmail.com>
2021-11-17 04:26:37 +0800

    add 896

---
+++ LeetCode/stack/85.js
@@ -0,0 +1,16 @@
+// https://leetcode-cn.com/problems/maximal-rectangle/
+
+// [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalRectangle = function (matrix) {};
+
+maximalRectangle([
+  ["1", "0", "1", "0", "0"],
+  ["1", "0", "1", "1", "1"],
+  ["1", "1", "1", "1", "1"],
+  ["1", "0", "0", "1", "0"],
+]); // 6
--- LeetCode/stack/896.js
+++ LeetCode/stack/896.js
@@ -9,41 +9,57 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var isMonotonic = function (nums) {
+//   if (nums.length < 3) {
+//     return true;
+//   }
+//   let r = nums[1] - nums[0],
+//     result = true;
+//   let cur = nums[1];
+//   for (let i = 2; i < nums.length; i++) {
+//     const cr = nums[i] - cur;
+//     // 前面是递减，现在是递增，说明是不是单调栈
+//     if (cr > 0 && r < 0) {
+//       result = false;
+//       break;
+//     }
+//     // 前面是递增，现在是递减，说明是不是单调栈
+//     if (cr < 0 && r > 0) {
+//       result = false;
+//       break;
+//     }
+
+//     // 如果前面是0，但是现在确定了单调性，则把单调性确定下
+//     if (r == 0 && cr != 0) {
+//       r = cr;
+//     }
+
+//     cur = nums[i];
+//   }
+
+//   //   if (r === 0) {
+//   //     console.log(result, "平衡");
+//   //   } else {
+//   //     console.log(result, r > 0 ? "递增" : "递减");
+//   //   }
+
+//   return result;
+// };
 var isMonotonic = function (nums) {
-  if (nums.length < 3) {
-    return true;
+  const len = nums.length - 1;
+  let inr = true,
+    desc = true;
+  for (var i = 0; i < len; i++) {
+    const cur = nums[i],
+      prev = nums[i + 1];
+    if (cur > prev) {
+      inr = false;
+    }
+    if (cur < prev) {
+      desc = false;
+    }
   }
-  let r = nums[1] - nums[0],
-    result = true;
-  let cur = nums[1];
-  for (let i = 2; i < nums.length; i++) {
-    const cr = nums[i] - cur;
-    // 前面是递减，现在是递增，说明是不是单调栈
-    if (cr > 0 && r < 0) {
-      result = false;
-      break;
-    }
-    // 前面是递增，现在是递减，说明是不是单调栈
-    if (cr < 0 && r > 0) {
-      result = false;
-      break;
-    }
-
-    // 如果前面是0，但是现在确定了单调性，则把单调性确定下
-    if (r == 0 && cr != 0) {
-      r = cr;
-    }
-
-    cur = nums[i];
-  }
-
-  //   if (r === 0) {
-  //     console.log(result, "平衡");
-  //   } else {
-  //     console.log(result, r > 0 ? "递增" : "递减");
-  //   }
-
-  return result;
+  return inr || desc;
 };
 isMonotonic([11, 11, 9, 4, 3, 3, 3, 1, -1, -1, 3, 3, 3, 5, 5, 5]);
 isMonotonic([1, 2, 2, 3]);
