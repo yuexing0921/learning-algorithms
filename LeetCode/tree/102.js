f170ad443b0e397df1292dd52aa8523de36fa19d
yuexing0921 <yuexing0921@gmail.com>
2021-11-21 21:58:53 +0800

    102

---
+++ LeetCode/tree/102.js
@@ -0,0 +1,35 @@
+// https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 层序遍历，关键是用队列（先入先出）
+ * 一层层遍历
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  const res = [];
+  const queue = [];
+  if (root) {
+    queue.push(root);
+  }
+  while (queue.length) {
+    const level = [];
+    let n = queue.length;
+    while (n--) {
+      const node = queue.shift();
+      level.push(node.val);
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+    res.push(level);
+  }
+  return res;
+};
