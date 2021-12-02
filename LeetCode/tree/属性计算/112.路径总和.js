fff7e89049df4de3813717fe863f282acdfce45f
yuexing0921 <yuexing0921@gmail.com>
2021-12-03 01:23:43 +0800

    112

---
+++ LeetCode/tree/属性计算/112.路径总和.js
@@ -0,0 +1,27 @@
+// https://leetcode-cn.com/problems/path-sum/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 112. 路径总和 - 递归法（最简单的）
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  if (!root) {
+    return false;
+  }
+  if (!root.left && !root.right) {
+    return targetSum - root.val === 0;
+  }
+  return (
+    hasPathSum(root.left, targetSum - root.val) ||
+    hasPathSum(root.right, targetSum - root.val)
+  );
+};
