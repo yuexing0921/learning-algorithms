33ffdd97ea12c166e6ad1eff0e9a541d53454b3a
yuexing0921 <yuexing0921@gmail.com>
2021-12-04 00:12:43 +0800

    复习

---
+++ LeetCode/tree/属性计算/113. 路径总和 II.js
@@ -0,0 +1,17 @@
+// https://leetcode-cn.com/problems/path-sum-ii/
+// 113. 路径总和 II.js
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function (root, targetSum) {};
