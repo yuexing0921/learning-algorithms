5547c3b85624f6b45bbfc0594384a44f1fb49cfe
yuexing0921 <yuexing0921@gmail.com>
2021-12-04 09:47:34 +0800

    selectionSort

---
+++ src/sort/selection.js
@@ -0,0 +1,35 @@
+// 选择排序算法
+/**
+ *原理是：O(n^2) 不稳定的排序方法
+ * 1. 从待排序的数据元素中选出最小的一个元素，存放在序列的起始位置
+ * 2. 再从剩余的未排序元素中寻找到最小（大）元素，然后放到已排序的序列的末尾
+ * 3. 以此类推，直到全部待排序的数据元素的个数为零
+ * @param {*} array
+ * @returns
+ */
+export function selectionSort(array) {
+  const len = array.length;
+  for (let i = 0; i < len; i++) {
+    let min = i;
+    for (let j = i + 1; j < len; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      swap(array, i, min);
+    }
+  }
+  return array;
+}
+/**
+ * 交换两个数组中的两个位置
+ * @param {*} array
+ * @param {*} idx
+ * @param {*} min
+ */
+let swap = function (array, idx, min) {
+  let aux = array[idx];
+  array[idx] = array[min];
+  array[min] = aux;
+};
---
+++ test/sort/selection.spec.js
@@ -0,0 +1,19 @@
+import { selectionSort } from "../../src/sort/selection";
+describe("选择排序", () => {
+  test("空数组排序", () => {
+    expect(selectionSort([]).length).toEqual(0);
+  });
+  test("单个元素排序", () => {
+    expect(selectionSort([1])).toEqual([1]);
+  });
+  test("2个元素排序", () => {
+    expect(selectionSort([3, 1])).toEqual([1, 3]);
+  });
+  test("多个个元素排序", () => {
+    expect(selectionSort([1, 3, 1])).toEqual([1, 1, 3]);
+  });
+
+  test("多个个元素排序", () => {
+    expect(selectionSort([1, 3, 4, 2, 1])).toEqual([1, 1, 2, 3, 4]);
+  });
+});
