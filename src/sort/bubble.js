ea8aa8e5d7566b2227d1b71cc3137fdbeb48c82d
yuexing0921 <yuexing0921@gmail.com>
2021-12-04 10:01:58 +0800

    bubble

--- src/sort/bubble.js
+++ src/sort/bubble.js
@@ -1,8 +1,22 @@
-// 
+//
 /**
- * 冒泡排序
+ * 冒泡排序: O(n^2) 稳定的排序方法
+ * 1. 要把相邻的元素两两进行比较，根据大小交换元素的位置
+ * 2. 每次都把大的元素往后挪
+ * 重复遍历这个过程
  * @param arr
  */
-function bubble(arr){
-    
-}   
+export function bubbleSort(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      // 每次都找到相连元素
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
