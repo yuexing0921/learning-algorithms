6484f33c238127bfc12823af475b27b8ed970813
yuexing0921 <yuexing0921@gmail.com>
2021-12-07 10:43:17 +0800

    restructuring tree

---
+++ LeetCode/tree/03二叉树的判断/958. 判断是否完全二叉树.js
@@ -0,0 +1,15 @@
+// https://leetcode-cn.com/problems/check-completeness-of-a-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 958. 判断是否完全二叉树
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isCompleteTree = function (root) {};
